cc7878534271302a175e3204daa6ca2ee49deb4a
ekaterina <kata_ndv@mail.ru>
2019-10-03 22:41:40 +0300

    Small spring+jsp test

---
+++ src/main/java/test/TestController.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping("")
+@Controller
+public class TestController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String test(ModelMap modelMap){
+        modelMap.addAttribute("message", "spring+jsp test");
+        return "test";
+    }
+
+}
